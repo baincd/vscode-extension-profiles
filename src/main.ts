f5e63552ed42d3a49f47ae4776b52f2c738b34fc
Chris Bain <baincd@users.noreply.github.com>
2021-06-19 09:45:40 -0400

    Refactor main class

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,50 @@
 import * as vscode from 'vscode';
 
-class ProfileQuickPickOption {
+class ExtProfile {
 	name: string;
-	enabled = false;
+	activated = false;
 	exists = true;
 	constructor(name: string) {
 		this.name = name;
 	}
 	displayName() {
-		return (this.enabled ? "(Active) " : "") + this.name + (!this.exists ? " (Profile Not Defined)" : "") ;
+		return (this.activated ? "(Active) " : "") + this.name + (!this.exists ? " (PROFILE NOT DEFINED)" : "") ;
 	}
 }
 
-function activeProfilesSetup() {
+function getExtProfiles() {
 	const config = vscode.workspace.getConfiguration("extension-profiles");
-	let options: ProfileQuickPickOption[] = [];
+	let extProfiles: ExtProfile[] = [];
 
-	Object.keys(config.get<any>("profiles")).forEach(key => options.push(new ProfileQuickPickOption(key)));
+	Object.keys(config.get<any>("profiles")).forEach(key => extProfiles.push(new ExtProfile(key)));
 	config.get<Array<string>>("activeProfiles")?.forEach(key => {
-		let opt = options.find(opt => opt.name == key);
+		let opt = extProfiles.find(opt => opt.name == key);
 		if (opt) {
-			opt.enabled = true;
+			opt.activated = true;
 		} else {
-			const newOpt = new ProfileQuickPickOption(key);
+			const newOpt = new ExtProfile(key);
 			newOpt.exists = false;
-			newOpt.enabled = true;
-			options.push(newOpt)
+			newOpt.activated = true;
+			extProfiles.push(newOpt);
 		}
 	});
 
-	options.sort((a,b) => {
-		if (a.enabled && !b.enabled) {
+	extProfiles.sort((a, b) => {
+		if (a.activated && !b.activated) {
 			return -1;
-		} else if (!a.enabled && b.enabled) {
+		} else if (!a.activated && b.activated) {
 			return 1;
 		} else {
-			return a.name.localeCompare(b.name,);
+			return a.name.localeCompare(b.name);
 		}
-	})
+	});
+	return extProfiles;
+}
 
-	if (options.length == 0) {
+function activeProfilesSetupCommand() {
+	let extProfiles: ExtProfile[] = getExtProfiles();
+
+	if (extProfiles.length == 0) {
 		vscode.window.showErrorMessage("No profiles defined!","Define profiles now")
 			.then(btnPressed => { 
 				if (btnPressed) {
@@ -51,10 +56,10 @@ function activeProfilesSetup() {
 	}
 
 
-	vscode.window.showQuickPick(options.map(opt => opt.displayName()))
+	vscode.window.showQuickPick(extProfiles.map(opt => opt.displayName()))
 		.then(selectedDisplayName => {
 			if (selectedDisplayName) {
-				const selectedOpt = options.find(opt => opt.displayName() == selectedDisplayName);
+				const selectedOpt = extProfiles.find(opt => opt.displayName() == selectedDisplayName);
 				if (selectedOpt) {
 					profileActionPicker(selectedOpt)
 				}
@@ -63,26 +68,26 @@ function activeProfilesSetup() {
 	)
 }
 
-function profileActionPicker(opt: ProfileQuickPickOption) {
+function profileActionPicker(opt: ExtProfile) {
 	let viewAction = "View extensions in " + opt.name + " profile";
 	let activateAction = "Activate " + opt.name + " profile";
 	let deactivateAction = "Deactivate " + opt.name + " profile";
 
-	let options: string[] = [];
+	let actionOptions: string[] = [];
 
-	if (opt.exists && !opt.enabled) {
-		options.push(activateAction);
+	if (opt.exists && !opt.activated) {
+		actionOptions.push(activateAction);
 	}
 	if (opt.exists) {
-		options.push(viewAction);
+		actionOptions.push(viewAction);
 	}
-	if (opt.enabled) {
-		options.push(deactivateAction);
+	if (opt.activated) {
+		actionOptions.push(deactivateAction);
 	}
 
-	vscode.window.showQuickPick(options)
-		.then(selectedOption => {
-			switch(selectedOption) {
+	vscode.window.showQuickPick(actionOptions)
+		.then(selectedAction => {
+			switch(selectedAction) {
 				case viewAction: profileAction(opt.name, false, opt.exists, false); break;
 				case activateAction: profileAction(opt.name, true, opt.exists, false); break;
 				case deactivateAction: profileAction(opt.name, false, opt.exists, true); break;
@@ -115,5 +120,5 @@ function profileAction(profileName: string, activate: boolean, view: boolean, de
 }
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.commands.registerCommand("extension-profiles.active-profiles-setup",activeProfilesSetup);
+	vscode.commands.registerCommand("extension-profiles.active-profiles-setup",activeProfilesSetupCommand);
 }
