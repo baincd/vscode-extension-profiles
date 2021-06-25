1006965c8bbd307eaec870f2a52335d3d6561a71
Chris Bain <baincd@users.noreply.github.com>
2021-06-24 20:21:11 -0400

    Make startup check a low priority

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as vscode from 'vscode';
 import { activeProfilesSetupCommand, activateProfileCommand, startupCheck } from "./actions";
 import { getConfig } from './configUtils';
 
+const MS_STARTUP_CHECK_DELAY = 250;
+
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand("extension-profiles.active-profiles-setup",activeProfilesSetupCommand)
@@ -14,6 +16,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const config = getConfig();
 	if (config.checkAllActiveProfileExtensionsAreEnabledOnStartup) {
-		startupCheck(config);
+		setTimeout(() => {startupCheck(config)}, MS_STARTUP_CHECK_DELAY);
 	}
 }
